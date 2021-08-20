782a9d6e0bc4e6e987b6a04926170cd524128fc8
Akansha <akanshaaggarwal9876@gmail.com>
2021-08-20 15:51:41 +0530

    Akansha Enterprises

---
+++ form-handler.php
@@ -0,0 +1,19 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'akanshaaggarwal9876@gmail.com';
+$email_subject = 'new form submission';
+$email_body = " user name: $name.\n".
+                  " user email: $visitor_email.\n".
+                  " subject: $subject.\n".
+                  " user message: $message.\n".;
+
+$to = "ashishaggarwal9876@gmail.com";
+$headers = "From: $email_form \r\n";
+$headers = "Reply-To: $visitor_email \r\n";
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
+?>
